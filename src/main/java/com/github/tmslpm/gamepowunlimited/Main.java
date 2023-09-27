fce50c26c78c1c45dbacb41d2eb2cc9406adf9c3
tmslpm <thomaslepomme@gmail.com>
2023-09-27 23:34:32 +0200

    setup github action

--- src/main/java/com/github/tmslpm/gamepowunlimited/Main.java
+++ src/main/java/com/github/tmslpm/gamepowunlimited/Main.java
@@ -7,8 +7,47 @@ public class Main {
     public final static Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        GameProcessAction.start(1);
-        //GameProcessCLI.start();
+        boolean action_github = false;
+        int position = 0;
+
+        for (String arg : args) {
+            switch (arg) {
+                case "action_github":
+                    action_github = true;
+                    break;
+                case "position_1":
+                    position = 1;
+                    break;
+                case "position_2":
+                    position = 2;
+                    break;
+                case "position_3":
+                    position = 3;
+                    break;
+                case "position_4":
+                    position = 4;
+                    break;
+                case "position_5":
+                    position = 5;
+                    break;
+                case "position_6":
+                    position = 6;
+                    break;
+                case "position_7":
+                    position = 7;
+                    break;
+            }
+        }
+
+        if (action_github) {
+            if (position > 0) GameProcessAction.start(1);
+            else throw new IllegalArgumentException("bad position, excepted 1 to 7");
+            System.out.println("Success called main, type: Github Action (position play: " + position + ")");
+        } else {
+            GameProcessCLI.start();
+            System.out.println("Success called main, type: CLI");
+        }
+
     }
 
 
